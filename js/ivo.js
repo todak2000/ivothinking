2adde4039bc39fb0fac69e5f58846f4991ab7113
Daniel Olagunju <todak2000@gmail.com>
2018-10-09 15:43:29 +0100

    new designs

---
+++ js/ivo.js
@@ -0,0 +1,37 @@
+   // SLICK  STARTS
+   $( document ).ready(function() {
+    $('.your-class').slick({
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+
+        $('.your-foot').slick({
+        dots:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+        
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+
+    });
